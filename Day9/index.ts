99ec1abc7e9fd15760c87c906a73b27b952bb2a3
Krishna <krishnayadav30082002@gmail.com>
2026-05-11 12:40:10 +0530

    Added Codes

--- Day9/index.ts
+++ Day9/index.ts
@@ -1,2 +1,11 @@
-let name ="Krishna";
-console.log(name);
+let name: string = "Krishna";
+let result: number | string;
+result = 45;
+result = "success";
+console.log(name, result);
+ 
+function add(a: number, b: number): string {
+    return "Sum: " + (a + b);
+}
+ 
+console.log(add(5, 10));
